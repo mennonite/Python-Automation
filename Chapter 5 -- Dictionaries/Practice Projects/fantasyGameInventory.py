d1c3f769a2a21cb283995349d237ec9e0fd2b55f
mennonite <martinlara02@gmail.com>
2019-09-17 14:03:11 -0500

    Chapter 5 exercises completed

---
+++ Chapter 5 -- Dictionaries/Practice Projects/fantasyGameInventory.py
@@ -0,0 +1,26 @@
+# inventory.py
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        item_total += v
+    print("Total number of items: " + str(item_total))
+
+displayInventory(stuff)
+
+print()
+
+# convert list to dictionary
+def addToInventory(inventory, addedItems):
+    for k in addedItems:
+        inventory.setdefault(k, 0)
+        inventory[k] += 1
+    return inventory
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
