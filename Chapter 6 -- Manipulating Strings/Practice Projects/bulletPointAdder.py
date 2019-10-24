e6029348caea4945d0ee9647f03c207a5ef9bdbf
mennonite <martinlara02@gmail.com>
2019-10-23 20:32:14 -0500

    completed the program

--- Chapter 6 -- Manipulating Strings/Practice Projects/bulletPointAdder.py
+++ Chapter 6 -- Manipulating Strings/Practice Projects/bulletPointAdder.py
@@ -5,6 +5,9 @@
 import pyperclip
 text = pyperclip.paste()
 
-# TODO: Separate lines and add stars.
-
+# Separate lines and add stars.
+lines = text.split('\n')
+for i in range(len(lines)):    # loop through all indexes in the "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
 pyperclip.copy(text)
