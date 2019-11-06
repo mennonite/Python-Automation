0b1f4789b9e2589d8b347f808a08c57cd1157e93
mennonite <martinlara02@gmail.com>
2019-11-06 14:16:07 -0600

    completed regexStrip function

--- Chapter 7 -- Pattern Matching with Regular Expressions/Practice Projects/regexStrip.py
+++ Chapter 7 -- Pattern Matching with Regular Expressions/Practice Projects/regexStrip.py
@@ -1,12 +1,18 @@
 import re
 
-def regexStrip(text, value = r'\s'):
-    stripRegex = re.compile(value)
-    return stripRegex.sub('', text)
+def regexStrip(text, value = r''):
+    if value == '':
+        stripRegex = re.compile(r'^(\s*)(.*)(\s)*$')
+        trim = stripRegex.search(text)
+        return trim.group(2)
+    else:
+        stripRegex = re.compile(value)
+        return stripRegex.sub('', text)
 
-userString = input('please give me a string to test:')
+userString = input('Please give me a string to test:')
 
 if userString == '':
     print('ok then')
 else:
-    print(regexStrip(userString))
+    char = input('Please input a character you would like to remove from your string.\n(If left blank it will remove spaces from the beginning and end of the string):\n')
+    print(regexStrip(userString, char))
