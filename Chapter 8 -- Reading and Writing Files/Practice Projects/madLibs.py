02e63448f021ce8e3bd95882b8ac3b11ba1f14c6
mennonite <martinlara02@gmail.com>
2019-11-12 22:01:51 -0600

    gotta finish saving the text to a new file.  I may redo this using regex sub function

--- Chapter 8 -- Reading and Writing Files/Practice Projects/madLibs.py
+++ Chapter 8 -- Reading and Writing Files/Practice Projects/madLibs.py
@@ -1,29 +1,30 @@
 #! python3
 # madLibs.py - opens and reads a text file and lets the user input their own words
 
-# TODO: search a text file for a word from the following list: [ADJECTIVE, NOUN, ADVERB, VERB]
+import re
 
-# open txt file
-madlibFile = open('test.txt')
+# open text file
+madlibFile = open('.\\Chapter 8 -- Reading and Writing Files\\Practice Projects\\test.txt')
 
 # save the content of the file into a variable
 
-content = madlibFile.read()
+content = madlibFile.read().split()
 madlibFile.close()
-
 searchList = ['ADJECTIVE', 'NOUN', 'ADVERB', 'VERB']
-for word in searchList:
-    for i in content.split():
-        if i.upper() == word and word == 'ADJECTIVE':
-            i = input('Enter an adjective:')
-            print(i)
-        elif i.upper() == word:
-            i = input('Enter a %s:' % word)
-            print(i)
 
+for i in range(len(content)):
+    # prompt the user to enter a [searched term] of their choice if [searched term] exists in text file
+    if content[i].strip('.') in searchList:
+        if content[i].startswith('A') and content[i].endswith('.'):
+            content[i] = input('Gimme an %s:\n' % content[i].strip('.').lower()) + '.'
+        elif content[i].startswith('A'):
+            content[i] = input('Gimme an %s:\n' % content[i].lower())
+        elif content[i].endswith('.'):
+            content[i] = input('Gimme a %s:\n' % content[i].strip('.').lower()) + '.'
+        else:
+            content[i] = input('Gimme a %s:\n' % content[i].lower())
 
-# TODO: prompt the user to enter a [searched term] of their choice
+print(' '.join(content))
 
-# TODO: the users term replaces the listed word in the text file and prints it to the screen
 
 # TODO: the resulting text is saved as a new file
