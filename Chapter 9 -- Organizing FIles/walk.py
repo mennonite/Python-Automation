fae4de8306a53e7221c9910ea976ca76c7055f0f
mennonite <martinlara02@gmail.com>
2019-11-14 21:03:27 -0600

    os.walk() function example program complete

---
+++ Chapter 9 -- Organizing FIles/walk.py
@@ -0,0 +1,15 @@
+#python!
+#walk.py - an example program that showcases the os.walk() function
+
+import os
+
+for folderName, subfolders, filenames in os.walk('C:\\Users\\Martin\\Desktop\\nuts'):
+    print('The current folder is ' + folderName)
+
+    for subfolder in subfolders:
+        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+
+    for filename in filenames:
+        print('FILE INSIDE ' + folderName + ': '+ filename)
+
+    print('')
