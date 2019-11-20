2276fb8ce334fe4003efa32de16e4b880c93eeec
mennonite <martinlara02@gmail.com>
2019-11-20 16:01:54 -0600

    example finished

--- Chapter 9 -- Organizing FIles/renameDates.py
+++ Chapter 9 -- Organizing FIles/renameDates.py
@@ -4,7 +4,7 @@
 
 import shutil, os, re
 
-# Create a regex that matches files with the American date format.
+# Create a regex that matches files with the American date format
 
 datePattern = re.compile(r'''^(.*?) # all text before the date
     ((0|1)?\d)-                     # one or two digits for the month
@@ -13,14 +13,29 @@ datePattern = re.compile(r'''^(.*?) # all text before the date
     (.*?)$                          # all text after the date
     ''', re.VERBOSE)
 
-# TODO: Loop over the files in the working directory
+# Loop over the files in the working directory
+for ameriFilename in os.listdir('.'):
+    mo = datePattern.search(ameriFilename)
 
-# TODO: Skip files without a date
+    # Skip files without the date
+    if mo == None:
+        continue
 
-# TODO: Get the different parts of the filename
+    # Get the different parts of the filename
+    beforePart = mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(4)
+    yearPart = mo.group(6)
+    afterPart = mo.group(8)
 
-# TODO: Form the European-style filename
+    # Form the European-style filename
+    euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
 
-# TODO: Get the full, absolute file paths
+    # Get the full, absolute file paths
+    absWorkingDir = os.path.abspath('.')
+    ameriFilename = os.path.join(absWorkingDir, ameriFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
 
-# TODO: Rename the files
+    # Rename the files
+    print('Renaming "%s" to "%s"...' % (ameriFilename, euroFilename))
+    #shutil.move(ameriFilename, euroFilename)   # uncomment after testing
