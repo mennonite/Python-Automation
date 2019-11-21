26934c5cf9d15ba42c1216f23e090975866adb0b
mennonite <martinlara02@gmail.com>
2019-11-20 19:47:46 -0600

    backup zip program example half way done

---
+++ Chapter 9 -- Organizing FIles/backupToZip.py
@@ -0,0 +1,26 @@
+#! python3
+# backupToZip.py - Copies an entire filder and its contents into
+# a ZIP file whose filename increments
+
+import zipfile, os
+
+def backupToZip(folder):
+    # Backup the entire contents of 'folder' into a ZIP file
+
+    folder = os.path.abspath(folder)    # make sure folder is absolute
+
+    # Figure out the filename this code should use based on
+    # what files already exist
+    number = 1
+    while True:
+        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        if not os.path.exists(zipFilename):
+            break
+        number = number + 1
+
+    # TODO: Create the ZIP file
+
+    # TODO: Walk the entire folder tree and compress the files in each folder
+    print('Done.')
+
+backupToZip(os.path.sep.join([os.getcwd().split(os.path.sep)[0], os.getcwd().split(os.path.sep)[1], os.getcwd().split(os.path.sep)[2], os.getcwd().split(os.path.sep)[3]]))
